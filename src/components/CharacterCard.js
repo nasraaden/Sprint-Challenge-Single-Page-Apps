98223fe83086ee4517da300a594474c7b7ffe6ac
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2019-11-15 10:37:00 -0800

    MVP and some styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Character = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const CharacterCard = props => {
+  
+  
+    return (  
+      
+      <div className="character-card">
+        <img src ={props.image}/>
+        <h2>{props.name}</h2>
+        <p>{`Gender: ${props.gender}`}</p>
+        <p>{`Species: ${props.species}`}</p>
+        <p>{`Status: ${props.status}`}</p>
+        <p>{`Location: ${props.location}`}</p>
+      </div>
+      
+    ) 
+  
 }
+
+export default CharacterCard;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
 export default function SearchForm() {
- 
+  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      const results = response.data.results.filter(character =>
+        character.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(results);
+    })
+    .catch(error => {
+      console.error("The data is not returned.", error)
+    })
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+     
+     <form>
+       <label htmlFor="search">Search</label>
+       <input id="search" type="text" name="searchfield" placeholder="Find a Character" onChange={handleChange} value={searchTerm}/>
+     </form>
+
+     <div className="character-list">
+    
+          {searchResults.map(character => {
+           return ( <CharacterCard key={character.id} name={character.name} gender={character.gender} species ={character.species} status={character.status} location={character.location.name} image={character.image}/>)
+          }
+          )}
+
+      </div>
+
     </section>
   );
 }
