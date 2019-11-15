9ca154a9e744cc71ce91346566daeffe1c8dc4e7
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2019-11-15 10:51:39 -0800

    finished styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,26 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
-const Character = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`
+
 
 const CharacterCard = props => {
-  
-  
+  const CharacterContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 2%;
+  `
+  const Character = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 2%;
+  `
+    
     return (  
+      <CharacterContainer>
       
-      <div className="character-card">
         <img src ={props.image}/>
-        <h2>{props.name}</h2>
-        <p>{`Gender: ${props.gender}`}</p>
-        <p>{`Species: ${props.species}`}</p>
-        <p>{`Status: ${props.status}`}</p>
-        <p>{`Location: ${props.location}`}</p>
-      </div>
-      
+        <Character>
+          <h2>{props.name}</h2>
+          <p>{`Gender: ${props.gender}`}</p>
+          <p>{`Species: ${props.species}`}</p>
+          <p>{`Status: ${props.status}`}</p>
+          <p>{`Location: ${props.location}`}</p>
+          </Character>
+      </CharacterContainer>
     ) 
+   
   
 }
 
